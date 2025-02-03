7a08f73a680cd54ba6e6b7cb1a4d7ca33c02f635
josephrebne <joey@chargeover.com>
2025-02-03 09:40:54 -0600

    finished

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            premium: true
+            cart: [],
+            premium: true,
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeFromCart(id) {
+            const index = this.cart.indexOf(id)
+            if (index > -1) {
+                // only splice array when item is found
+                this.cart.splice(index, 1) // 2nd parameter means remove one item only
+            }
+        },
+    },
 })
