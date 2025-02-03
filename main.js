6f731da919fe6b29bfa396f4e02b3f5ace401aef
josephrebne <joey@chargeover.com>
2025-02-03 09:11:35 -0600

    finished

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
+            onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 9 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
+        updateVariant(index) {
+            this.selectedVariant = index
+            this.onSale = this.variants[index].quantity < 10 && this.variants[index].quantity > 0
+        },
+    },
+    computed: {
+        title() {
+            return `${this.brand} ${this.product}`
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0
+        },
+        sale() {
+            return `${this.title} is on sale!`
+        },
+    },
 })
